fc2f481a7d0ae4f362577995fe5a82eeed972896
milad <milad.yeganeh@gmail.com>
2019-06-29 13:39:13 +0430

    Deploying-War-File

--- src/main/java/com/milad/ws/restconcept/RestconceptApplication.java
+++ src/main/java/com/milad/ws/restconcept/RestconceptApplication.java
@@ -2,14 +2,21 @@ package com.milad.ws.restconcept;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class RestconceptApplication {
+public class RestconceptApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(RestconceptApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return super.configure(builder);
     }
 
     @Bean
